af7050f5a1ec81c754641409647dba4ffbdc9b2d
mhankbe <49732882+mhankbe@users.noreply.github.com>
2026-07-17 04:17:18 +0700

    Update 2_CONFIG_SINGLE_SELECT_GETER_4_FIX (2).lua

--- 2_CONFIG_SINGLE_SELECT_GETER_4_FIX (2).lua
+++ 2_CONFIG_SINGLE_SELECT_GETER_4_FIX (2).lua
@@ -2751,9 +2751,9 @@ do
                         if (tick() - last) >= 0.001 then
                             _lastFire[hGuid] = tick()
                             if RE.HeroUseSkill then
-                                pcall(function() RE.HeroUseSkill:FireServer({heroGuid=hGuid,attackType=1,userId=MY_USER_ID,enemyGuid=g}) end)
+                                pcall(function() RE.HeroUseSkill:FireServer({heroGuid=hGuid,attackType=3,userId=MY_USER_ID,enemyGuid=g}) end)
                                 task.wait(0.001)
-                                pcall(function() RE.HeroUseSkill:FireServer({heroGuid=hGuid,attackType=2,userId=MY_USER_ID,enemyGuid=g}) end)
+                                pcall(function() RE.HeroUseSkill:FireServer({heroGuid=hGuid,attackType=3,userId=MY_USER_ID,enemyGuid=g}) end)
                                 task.wait(0.001)
                                 pcall(function() RE.HeroUseSkill:FireServer({heroGuid=hGuid,attackType=3,userId=MY_USER_ID,enemyGuid=g}) end)
                             end
